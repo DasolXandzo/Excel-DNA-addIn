91634b1a5eae8069fa8eda4b9eca85e637130f36
Xodzi <ivan.kotelnikov96@mail.ru>
2024-01-12 21:40:01 +0300

    FixDepth

--- Excel DNA/Class1.cs
+++ Excel DNA/Class1.cs
@@ -16,108 +16,29 @@ namespace Excel_DNA
         public string? Name { get; set; }
         public string? Depth { get; set; }
         public string? Result { get; set; }
+        public List<ParseTreeNode>? ChildNodes { get; set; }
     }
-    public static class MyFunctions
+    public class NodeDepth
     {
-        static List<Node> res = new List<Node>();
-
-        [ExcelCommand(MenuName = "Test", MenuText = "Range Set")]
-        public static void RangeGet()
-        {
-            res.Clear();
-            Microsoft.Office.Interop.Excel.Application excelApp = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application; 
-            Microsoft.Office.Interop.Excel.Range range = excelApp.ActiveCell;
-            //var res2 = excelApp.Evaluate("A1");
-
-            ParseTreeNode node =  ExcelFormulaParser.Parse(range.Formula);
-            FormulaAnalyzer analyzer = new FormulaAnalyzer(range.Formula);
-            DepthFirstSearch(node, excelApp);
-            var leters = JsonSerializer.Serialize(res[0].Name);
-            var json = JsonSerializer.Serialize(res);
-            var url = "http://localhost:3000/?dialogID=15&lettersFormula=" + leters + "&valuesFormula = " + leters + "&jsonString=" + json;
-            MyForm form = new MyForm(url);
-            form.Show();
-
-        }
-        public static void DepthFirstSearch(ParseTreeNode root, Microsoft.Office.Interop.Excel.Application application)
-        {
-            if (root == null)
-                return;
-
-            if (root.IsFunction())
-            {
-                root.Print();
-                FormulaAnalyzer analyzer = new FormulaAnalyzer(root);
-                var name = root.Print();
-                var depth = analyzer.Depth().ToString();
-                //var wb = application.Workbooks.Add(Type.Missing);
-                //var ws = wb.Worksheets[1];
-                //application.Windows[1].Visible= false;
-                var result = RangeSet("="+name);
-                res.Add(new Node{ Name = name, Depth = depth, Result = result});
-                var stop = 5;
-                foreach (var child in root.ChildNodes)
-                {
-                    DepthFirstSearch(child, application);
-                }
-                return;
-            }
-            if(root.IsRange())
-            {
-                FormulaAnalyzer analyzer = new FormulaAnalyzer(root);
-                var name = root.Print();
-                var depth = analyzer.Depth().ToString();
-                var result = "range";
-                res.Add(new Node { Name = name, Depth = depth, Result = result });
-                foreach (var child in root.ChildNodes)
-                {
-                    DepthFirstSearch(child, application);
-                }
-                return;
-            }
-            if (root.IsParentheses())
-            {
-                FormulaAnalyzer analyzer = new FormulaAnalyzer(root);
-                var name = root.Print();
-                var depth = analyzer.Depth().ToString();
-                var result = RangeSet("=" + name);
-                res.Add(new Node { Name = name, Depth = depth, Result = result });
-                foreach (var child in root.ChildNodes)
-                {
-                    DepthFirstSearch(child, application);
-                }
-                return;
-            }
-            
-
-            foreach (var child in root.ChildNodes)
-            {
-                DepthFirstSearch(child, application);
-            }
-        }
-
-        public static string RangeSet(string formula)
-        {
-            Microsoft.Office.Interop.Excel.Application excelApp = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application;
-            excelApp.Range["K13"].Formula = formula;
-            Microsoft.Office.Interop.Excel.Range range = excelApp.Range["K13"];
-            return range.Value.ToString();
-            var test = 5;
-        }
+        public ParseTreeNode? Node { get; set; }
+        public int? Depth { set; get; }
     }
+
+
 
     [ComVisible(true)]
-    public class RibbonController : ExcelRibbon
+    public class MyFunctions: ExcelRibbon
     {
+        static List<Node> res = new List<Node>();
         public override string GetCustomUI(string RibbonID)
         {
             return @"
       <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
       <ribbon>
         <tabs>
-          <tab id='tab1' label='My Tab'>
-            <group id='group1' label='My Group'>
-              <button id='button1' label='My Button' onAction='OnButtonPressed'/>
+          <tab id='tab1' label='Дерево'>
+            <group id='group1' label='Надстройка'>
+              <button id='button1' label='Создать дерево' onAction='OnButtonPressed'/>
             </group >
           </tab>
         </tabs>
@@ -130,6 +51,88 @@ namespace Excel_DNA
             //var url = "https://test-excel.vercel.app/?dialogID=15&lettersFormula=" + res
             //MyForm form = new MyForm();
             //form.Show();
+            RangeGet();
         }
+        public static void RangeGet()
+        {
+            res.Clear();
+            Microsoft.Office.Interop.Excel.Application excelApp = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application; 
+            Microsoft.Office.Interop.Excel.Range range = excelApp.ActiveCell;
+            //var res2 = excelApp.Evaluate("A1");
+
+            ParseTreeNode node =  ExcelFormulaParser.Parse(range.Formula);
+            FormulaAnalyzer analyzer = new FormulaAnalyzer(range.Formula);
+            DepthFirstSearch(node, excelApp, 0);
+            var leters = JsonSerializer.Serialize(res[0].Name);
+            var json = JsonSerializer.Serialize(res);
+            var url = "http://localhost:3000/?dialogID=15&lettersFormula=" + leters + "&valuesFormula = " + leters + "&jsonString=" + json;
+            MyForm form = new MyForm(url);
+            form.Show();
+
+        }
+        public static void DepthFirstSearch(ParseTreeNode root, Microsoft.Office.Interop.Excel.Application application, int depth)
+        {
+            if (root.IsFunction())
+            {
+                root.Print();
+                FormulaAnalyzer analyzer = new FormulaAnalyzer(root);
+                var name = root.Print();
+                //var depth = analyzer.Depth().ToString();
+                //var wb = application.Workbooks.Add(Type.Missing);
+                //var ws = wb.Worksheets[1];
+                //application.Windows[1].Visible= false;
+                var result = RangeSet("="+name);
+                res.Add(new Node{ Name = name, Depth = depth.ToString(), Result = result});
+                var stop = 5;
+                foreach (var child in root.ChildNodes)
+                {
+                    DepthFirstSearch(child, application, depth + 1);
+                }
+                return;
+            }
+            if(root.IsRange())
+            {
+                FormulaAnalyzer analyzer = new FormulaAnalyzer(root);
+                var name = root.Print();
+               // var depth = analyzer.Depth().ToString();
+                var result = "range";
+                res.Add(new Node { Name = name, Depth = depth.ToString(), Result = result });
+                foreach (var child in root.ChildNodes)
+                {
+                    DepthFirstSearch(child, application, depth + 1);
+                }
+                return;
+            }
+            if (root.IsParentheses())
+            {
+                FormulaAnalyzer analyzer = new FormulaAnalyzer(root);
+                var name = root.Print();
+              //  var depth = analyzer.Depth().ToString();
+                var result = RangeSet("=" + name);
+                res.Add(new Node { Name = name, Depth = depth.ToString(), Result = result });
+                foreach (var child in root.ChildNodes)
+                {
+                    DepthFirstSearch(child, application, depth + 1);
+                }
+                return;
+            }
+            
+
+            foreach (var child in root.ChildNodes)
+            {
+                DepthFirstSearch(child, application, depth);
+            }
+        }
+
+        public static string RangeSet(string formula)
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application;
+            excelApp.Range["K13"].Formula = formula;
+            Microsoft.Office.Interop.Excel.Range range = excelApp.Range["K13"];
+            return range.Value.ToString();
+            var test = 5;
+        }
+
     }
+
 }
